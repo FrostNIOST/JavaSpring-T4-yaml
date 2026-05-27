9f368cbc3cb1df0d35f301d593e57fe638992a80
Santiago Felipe Martinez Pineda <149542358+FrostNIOST@users.noreply.github.com>
2026-05-27 14:21:11 -0500

    chore: integracion mapstruct a tipo documento

--- src/main/java/co/edu/sena/project_yaml/services/mapper/TipoDocumentoMapper.java
+++ src/main/java/co/edu/sena/project_yaml/services/mapper/TipoDocumentoMapper.java
@@ -2,10 +2,15 @@ package co.edu.sena.project_yaml.services.mapper;
 
 import co.edu.sena.project_yaml.domain.TipoDocumento;
 import co.edu.sena.project_yaml.services.dto.TipoDocumentoDTO;
+import com.mongodb.internal.logging.LogMessage;
+import org.mapstruct.Mapper;
 
+import javax.swing.*;
 import java.util.List;
 
-public interface TipoDocumentoMapper {
+
+@Mapper(componentModel = "spring")
+public interface TipoDocumentoMapper extends EntityMapper<TipoDocumentoDTO, TipoDocumento>{
     TipoDocumento toEntity (TipoDocumentoDTO tipoDocumentoDTO);
     TipoDocumentoDTO toDto (TipoDocumento tipoDocumento);
     List<TipoDocumentoDTO> toDto(List<TipoDocumento> tipoDocumentos);
